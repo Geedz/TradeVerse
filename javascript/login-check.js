9579a6a75d56e9b43b48822d69b6905a817a31a2
Geedz <95632184+Geedz@users.noreply.github.com>
2024-11-24 14:48:00 +0100

    Update login-check.js

--- javascript/login-check.js
+++ javascript/login-check.js
@@ -2,7 +2,7 @@ function setCookie(name, value, days) {
   let expires = "";
   if (days) {
       const date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      date.setTime(date.getTime() + (days * 60 * 60 * 1000));
       expires = "; expires=" + date.toUTCString();
   }
   document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=.tradeverse.it; Secure; SameSite=None";
